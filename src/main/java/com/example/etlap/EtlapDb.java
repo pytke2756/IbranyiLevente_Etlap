825d6fe95f2636182f18321402803c092d5419c6
Ibrányi Levente <ibranyilevente@gmail.com>
2022-01-19 15:01:00 +0100

    EtlapDb létrehozva

--- src/main/java/com/example/etlap/EtlapDb.java
+++ src/main/java/com/example/etlap/EtlapDb.java
@@ -1,2 +1,13 @@
-package com.example.etlap;public class EtlapDb {
+package com.example.etlap;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class EtlapDb {
+    Connection connection;
+
+    public EtlapDb() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/etlapdb", "root", "");
+    }
 }
