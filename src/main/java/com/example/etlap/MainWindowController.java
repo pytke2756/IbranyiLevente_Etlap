5bb43822fc46f4d2af8cac2fdf23cdcb7d4b6a02
Ibrányi Levente <ibranyilevente@gmail.com>
2022-01-19 16:33:25 +0100

    félkész

--- src/main/java/com/example/etlap/MainWindowController.java
+++ src/main/java/com/example/etlap/MainWindowController.java
@@ -1,14 +1,53 @@
 package com.example.etlap;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class MainWindowController {
     @FXML
-    private Label welcomeText;
-
+    private TableView<Etel> etelTable;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TableColumn<Etel, String> colNev;
+    @FXML
+    private TableColumn<Etel, Integer> colAr;
+    @FXML
+    private TableColumn<Etel, String> colKategoria;
+
+    private EtlapDb db;
+
+
+
+    public void initialize(){
+        colNev.setCellValueFactory(new PropertyValueFactory<>("nev"));
+        colAr.setCellValueFactory(new PropertyValueFactory<>("ar"));
+        colKategoria.setCellValueFactory(new PropertyValueFactory<>("kategoria"));
+
+        try {
+            db = new EtlapDb();
+            feltolt();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
+    
+    
+    private void feltolt(){
+        try {
+            List<Etel> etelLista = db.getEtelek();
+            etelTable.getItems().clear();
+            for (Etel item :
+                    etelLista) {
+                etelTable.getItems().add(item);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+
 }
