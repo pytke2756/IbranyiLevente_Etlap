b386ed294e4ba753df224a59d40f62c6a4378766
Ibrányi Levente <ibranyilevente@gmail.com>
2022-01-19 15:54:34 +0100

    Hello controller átnevezve

--- src/main/java/com/example/etlap/MainWindowController.java
+++ src/main/java/com/example/etlap/MainWindowController.java
@@ -3,7 +3,7 @@ package com.example.etlap;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainWindowController {
     @FXML
     private Label welcomeText;
 
