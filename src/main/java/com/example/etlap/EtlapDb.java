3a7dc2bf586da73eadb7df0cbdb2820d3409c1b8
Ibrányi Levente <ibranyilevente@gmail.com>
2022-01-19 16:32:59 +0100

    összes adat lekérése

--- src/main/java/com/example/etlap/EtlapDb.java
+++ src/main/java/com/example/etlap/EtlapDb.java
@@ -1,13 +1,30 @@
 package com.example.etlap;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EtlapDb {
     Connection connection;
 
     public EtlapDb() throws SQLException {
         connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/etlapdb", "root", "");
+    }
+
+    public List<Etel> getEtelek() throws SQLException {
+        List<Etel> etelek = new ArrayList<>();
+        Statement stmt = connection.createStatement();
+        String sql = "SELECT * FROM etlap;";
+        ResultSet result = stmt.executeQuery(sql);
+        while (result.next()) {
+            int id = result.getInt("id");
+            String nev = result.getString("nev");
+            String leiras = result.getString("leiras");
+            int ar = result.getInt("ar");
+            String kategoria = result.getString("kategoria");
+            Etel obj = new Etel(id,nev,leiras,ar,kategoria);
+            etelek.add(obj);
+        }
+        return etelek;
     }
 }
