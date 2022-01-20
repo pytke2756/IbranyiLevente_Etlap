224f71c2fef69f3c72ce61e70514cbf09800478d
Levente Ibrányi <ibranyilevente@gmail.com>
2022-01-20 14:37:16 +0100

    Alap méret változtatás, fő ablak címe átírva

--- src/main/java/com/example/etlap/EtlapApp.java
+++ src/main/java/com/example/etlap/EtlapApp.java
@@ -11,8 +11,8 @@ public class EtlapApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(EtlapApp.class.getResource("main_window.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 530);
+        stage.setTitle("ÉtelApp");
         stage.setScene(scene);
         stage.show();
     }
