c3b0d547a4f6e79a3b03faaa3a4684849876afde
Levente Ibrányi <ibranyilevente@gmail.com>
2022-01-29 18:24:26 +0100

    Kategória hozzádás ablak kezelése

--- src/main/java/com/example/etlap/KategoriaHozzadWindowController.java
+++ src/main/java/com/example/etlap/KategoriaHozzadWindowController.java
@@ -1,4 +1,31 @@
 package com.example.etlap;
 
-public class KategoriaHozzadWindowController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.sql.SQLException;
+
+public class KategoriaHozzadWindowController extends Controller{
+    @FXML
+    private TextField kategoriaNevInput;
+
+    private EtlapDb db;
+
+    public void initialize() throws SQLException {
+        db = new EtlapDb();
+    }
+
+    @FXML
+    public void kategoriaHozzaadasButtonClick(ActionEvent actionEvent) {
+        String ujKategoria = kategoriaNevInput.getText().toString().trim();
+        try {
+            db.kategoriaAdd(ujKategoria);
+            return;
+        } catch (SQLException e) {
+            alert("„Az adott névvel már létezik kategória");
+            return;
+        }
+    }
 }
