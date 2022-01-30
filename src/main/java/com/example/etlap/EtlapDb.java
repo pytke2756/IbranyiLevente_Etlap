9aea5d628471cae4eed752603f7460cdb5595173
Levente Ibrányi <ibranyilevente@gmail.com>
2022-01-30 13:55:16 +0100

    Kategória hozzáadása adatbáziban, étel lekérdezés módosítása

--- src/main/java/com/example/etlap/EtlapDb.java
+++ src/main/java/com/example/etlap/EtlapDb.java
@@ -23,19 +23,20 @@ public class EtlapDb {
             String leiras = result.getString("leiras");
             int ar = result.getInt("ar");
             String kategoria = result.getString("kategoriak.nev");
-            Etel obj = new Etel(id,nev,leiras,ar, kategoria);
+            int kategoriaId = result.getInt("kategoriak.id");
+            Etel obj = new Etel(id,nev,leiras,ar, kategoria, kategoriaId);
             etelek.add(obj);
         }
         return etelek;
     }
 
-    public int etelAdd(String nev, String leiras, int ar, String kategoria) throws SQLException {
-        String sql = "INSERT INTO etlap(nev, leiras, ar, kategoria) VALUES(?,?,?,?)";
+    public int etelAdd(String nev, String leiras, int ar, int kategoria) throws SQLException {
+        String sql = "INSERT INTO etlap(nev, leiras, ar, kategoria_id) VALUES(?,?,?,?)";
         PreparedStatement stmt = connection.prepareStatement(sql);
         stmt.setString(1, nev);
         stmt.setString(2, leiras);
         stmt.setInt(3, ar);
-        stmt.setString(4, kategoria);
+        stmt.setInt(4, kategoria);
         return stmt.executeUpdate();
     }
 
@@ -52,13 +53,13 @@ public class EtlapDb {
                 "nev = ?," +
                 "leiras = ?," +
                 "ar = ?," +
-                "kategoria = ? " +
+                "kategoria_id = ? " +
                 "WHERE id = ?";
         PreparedStatement stmt = connection.prepareStatement(sql);
         stmt.setString(1, modositando.getNev());
         stmt.setString(2, modositando.getLeiras());
         stmt.setInt(3, hozzaadottAr);
-        stmt.setString(4, modositando.getKategoria());
+        stmt.setInt(4, modositando.getKategoriaId());
         stmt.setInt(5, modositando.getId());
         int erintettSorok = stmt.executeUpdate();
         return erintettSorok == 1;
@@ -85,5 +86,13 @@ public class EtlapDb {
         PreparedStatement stmt = connection.prepareStatement(sql);
         stmt.setString(1, ujKategoria);
         return stmt.executeUpdate();
+    }
+
+    public boolean kategoriaTorles(int id) throws SQLException {
+        String sql = "DELETE FROM kategoriak WHERE id = ?";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        stmt.setInt(1, id);
+        int erintettSorok = stmt.executeUpdate();
+        return erintettSorok == 1;
     }
 }
